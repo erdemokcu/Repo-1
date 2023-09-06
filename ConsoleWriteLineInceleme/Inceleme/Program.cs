f23865510c8306a5718f578fd616b785d4ec50ce
Erdem Okcu <erdemokcu@outlook.com>
2023-09-06 07:04:09 +0300

    Diğer Komutlarla Çalıştım
    Write, WriteLine ve Readline komutlarını çalıştım aynı zamanda satır satır inceleme konusunu öğrendim.

---
+++ ConsoleWriteLineInceleme/Inceleme/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inceleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ekrana kullanıcının görmesi için bir yazı yazdırmak istediğimizde bu komutları kullanırız.
+            Console.Write("Merhaba Dünya ");
+            Console.WriteLine("Ben C# öğreniyorum.");
+
+            Console.WriteLine("Merhaba Dünya ");
+            Console.WriteLine("Ben C# öğreniyorum.");
+
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
